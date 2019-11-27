3373189d127ef7508709eb0012ef4f0bbf211c0b
Nick Nisi <nick@nisi.org>
2019-11-27 06:37:06 -0600

    refactor checkbox tests to use assertionTemplate
    - use assertionTemplate
    - remove unnecessary duplication
    - use comparators from test-helpers

--- src/checkbox/tests/unit/Checkbox.spec.tsx
+++ src/checkbox/tests/unit/Checkbox.spec.tsx
@@ -1,5 +1,7 @@
 const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
+import assertionTemplate from '@dojo/framework/testing/assertionTemplate';
+import { compareId, compareForId } from '../../../common/tests/support/test-helpers';
 
 import { tsx, create } from '@dojo/framework/core/vdom';
 import * as sinon from 'sinon';
@@ -10,17 +12,6 @@ import Checkbox from '../../index';
 import * as css from '../../../theme/checkbox.m.css';
 import { noop, stubEvent } from '../../../common/tests/support/test-helpers';
 import focus from '@dojo/framework/core/middleware/focus';
-
-const compareId = {
-	selector: 'input',
-	property: 'id',
-	comparator: (property: any) => typeof property === 'string'
-};
-const compareForId = {
-	selector: '@label',
-	property: 'forId',
-	comparator: (property: any) => typeof property === 'string'
-};
 
 const middlewareFactory = create();
 function createMockFocusMiddleware({
@@ -36,24 +27,42 @@ function createMockFocusMiddleware({
 		}))();
 }
 
-const expected = function(label = false, checked = false) {
-	return (
+const expected = ({
+	label = false,
+	checked = false,
+	disabled = undefined,
+	focused = false,
+	valid = undefined,
+	readOnly = undefined,
+	required = undefined
+}: { [key: string]: boolean | undefined } = {}) =>
+	assertionTemplate(() => (
 		<div
 			key="root"
-			classes={[css.root, checked ? css.checked : null, null, null, null, null, null, null]}
+			classes={[
+				css.root,
+				checked ? css.checked : null,
+				disabled ? css.disabled : null,
+				focused ? css.focused : null,
+				valid === false ? css.invalid : null,
+				valid === true ? css.valid : null,
+				readOnly ? css.readonly : null,
+				required ? css.required : null
+			]}
 		>
 			<div classes={css.inputWrapper}>
 				<input
+					assertion-key="input"
 					id=""
 					classes={css.input}
 					checked={checked}
-					disabled={undefined}
-					focus={false}
-					aria-invalid={null}
+					disabled={disabled}
+					focus={focused}
+					aria-invalid={valid === false ? 'true' : null}
 					name={undefined}
-					readonly={undefined}
-					aria-readonly={null}
-					required={undefined}
+					readonly={readOnly}
+					aria-readonly={readOnly === true ? 'true' : null}
+					required={required}
 					type="checkbox"
 					value={undefined}
 					onblur={noop}
@@ -69,12 +78,12 @@ const expected = function(label = false, checked = false) {
 					key="label"
 					theme={undefined}
 					classes={undefined}
-					disabled={undefined}
+					disabled={disabled}
 					focused={false}
 					hidden={undefined}
-					valid={undefined}
-					readOnly={undefined}
-					required={undefined}
+					valid={valid}
+					readOnly={readOnly}
+					required={required}
 					forId=""
 					secondary={true}
 				>
@@ -82,14 +91,13 @@ const expected = function(label = false, checked = false) {
 				</Label>
 			) : null}
 		</div>
-	);
-};
+	));
 
 registerSuite('Checkbox', {
 	tests: {
 		'default properties'() {
 			const h = harness(() => <Checkbox />, [compareId]);
-			h.expect(() => expected());
+			h.expect(expected());
 		},
 
 		'custom properties'() {
@@ -105,42 +113,17 @@ registerSuite('Checkbox', {
 				[compareId]
 			);
 
-			h.expect(() => (
-				<div
-					key="root"
-					classes={[css.root, css.checked, null, null, null, null, null, null]}
-				>
-					<div classes={css.inputWrapper}>
-						<input
-							id=""
-							aria-describedby="foo"
-							name="bar"
-							classes={css.input}
-							checked={true}
-							disabled={undefined}
-							focus={false}
-							aria-invalid={null}
-							readonly={undefined}
-							aria-readonly={null}
-							required={undefined}
-							type="checkbox"
-							value={undefined}
-							onblur={noop}
-							onchange={noop}
-							onfocus={noop}
-							onpointerenter={noop}
-							onpointerleave={noop}
-						/>
-						<div classes={css.background} />
-					</div>
-				</div>
-			));
+			h.expect(
+				expected({ checked: true })
+					.setProperty('~input', 'aria-describedby', 'foo')
+					.setProperty('~input', 'name', 'bar')
+			);
 		},
 
 		label() {
 			const h = harness(() => <Checkbox label="foo" />, [compareId, compareForId]);
 
-			h.expect(() => expected(true));
+			h.expect(expected({ label: true }));
 		},
 
 		'state classes'() {
@@ -160,76 +143,14 @@ registerSuite('Checkbox', {
 				[compareForId, compareId]
 			);
 
-			h.expect(() => (
-				<div
-					key="root"
-					classes={[
-						css.root,
-						null,
-						css.disabled,
-						null,
-						css.invalid,
-						null,
-						css.readonly,
-						css.required
-					]}
-				>
-					<div classes={css.inputWrapper}>
-						<input
-							id=""
-							classes={css.input}
-							checked={false}
-							focus={false}
-							aria-invalid="true"
-							aria-readonly="true"
-							type="checkbox"
-							value={undefined}
-							name={undefined}
-							onblur={noop}
-							onchange={noop}
-							onfocus={noop}
-							disabled={true}
-							readonly={true}
-							required={true}
-							onpointerenter={noop}
-							onpointerleave={noop}
-						/>
-						<div classes={css.background} />
-					</div>
-				</div>
-			));
+			h.expect(expected({ valid, disabled, readOnly, required }));
 
 			valid = true;
 			disabled = false;
 			readOnly = false;
 			required = false;
 
-			h.expect(() => (
-				<div key="root" classes={[css.root, null, null, null, null, css.valid, null, null]}>
-					<div classes={css.inputWrapper}>
-						<input
-							id=""
-							classes={css.input}
-							checked={false}
-							focus={false}
-							aria-invalid={null}
-							aria-readonly={null}
-							type="checkbox"
-							value={undefined}
-							name={undefined}
-							onblur={noop}
-							onchange={noop}
-							onfocus={noop}
-							disabled={false}
-							readonly={false}
-							required={false}
-							onpointerenter={noop}
-							onpointerleave={noop}
-						/>
-						<div classes={css.background} />
-					</div>
-				</div>
-			));
+			h.expect(expected({ valid, disabled, readOnly, required }));
 		},
 
 		'state properties on label'() {
@@ -246,59 +167,15 @@ registerSuite('Checkbox', {
 				[compareId, compareForId]
 			);
 
-			h.expect(() => (
-				<div
-					key="root"
-					classes={[
-						css.root,
-						null,
-						css.disabled,
-						null,
-						css.invalid,
-						null,
-						css.readonly,
-						css.required
-					]}
-				>
-					<div classes={css.inputWrapper}>
-						<input
-							disabled={true}
-							classes={css.input}
-							focus={false}
-							aria-invalid="true"
-							readonly={true}
-							aria-readonly="true"
-							required={true}
-							checked={false}
-							name={undefined}
-							type="checkbox"
-							value={undefined}
-							id=""
-							onblur={noop}
-							onchange={noop}
-							onfocus={noop}
-							onpointerenter={noop}
-							onpointerleave={noop}
-						/>
-						<div classes={css.background} />
-					</div>
-					<Label
-						key="label"
-						disabled={true}
-						focused={false}
-						theme={undefined}
-						classes={undefined}
-						readOnly={true}
-						required={true}
-						valid={false}
-						hidden={undefined}
-						forId=""
-						secondary={true}
-					>
-						foo
-					</Label>
-				</div>
-			));
+			h.expect(
+				expected({
+					label: true,
+					disabled: true,
+					readOnly: true,
+					required: true,
+					valid: false
+				})
+			);
 		},
 
 		'focused class'() {
@@ -311,35 +188,7 @@ registerSuite('Checkbox', {
 				middleware: [[focus, focusMock]],
 				customComparator: [compareId]
 			});
-			h.expect(() => (
-				<div
-					key="root"
-					classes={[css.root, null, null, css.focused, null, null, null, null]}
-				>
-					<div classes={css.inputWrapper}>
-						<input
-							id=""
-							classes={css.input}
-							checked={false}
-							disabled={undefined}
-							focus={true}
-							aria-invalid={null}
-							name={undefined}
-							readonly={undefined}
-							aria-readonly={null}
-							required={undefined}
-							type="checkbox"
-							value={undefined}
-							onblur={noop}
-							onchange={noop}
-							onfocus={noop}
-							onpointerenter={noop}
-							onpointerleave={noop}
-						/>
-						<div classes={css.background} />
-					</div>
-				</div>
-			));
+			h.expect(expected({ focused: true }));
 		},
 
 		events() {
